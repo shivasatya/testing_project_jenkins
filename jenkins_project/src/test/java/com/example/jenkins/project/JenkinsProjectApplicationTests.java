c3e6729e0eebca066532588d1811c915765caeec
shivasatya <65852070+shivasatya@users.noreply.github.com>
2023-01-22 13:49:30 +0530

    Add files via upload
    few

---
+++ jenkins_project/src/test/java/com/example/jenkins/project/JenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
